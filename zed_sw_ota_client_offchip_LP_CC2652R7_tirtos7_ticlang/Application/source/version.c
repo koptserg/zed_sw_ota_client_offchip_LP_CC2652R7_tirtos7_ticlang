4ddc1f89af9e4eb510db6ec03e71822776a30a1b
koptserg <kopt@realsystem.ru>
2024-10-21 23:33:58 +0500

    123

--- zed_sw_ota_client_offchip_LP_CC2652R7_tirtos7_ticlang/Application/source/version.c
+++ zed_sw_ota_client_offchip_LP_CC2652R7_tirtos7_ticlang/Application/source/version.c
@@ -7,8 +7,8 @@
     #endif
 
     #include "version.h"
-const uint8_t zclApp_DateCode[] = { 16, '2', '1', '/', '1', '0', '/', '2', '0', '2', '4', ' ', '2', '2', ':', '4', '1' };
-const char zclApp_DateCodeNT[] = "21/10/2024 22:41";
+const uint8_t zclApp_DateCode[] = { 16, '2', '1', '/', '1', '0', '/', '2', '0', '2', '4', ' ', '2', '3', ':', '3', '1' };
+const char zclApp_DateCodeNT[] = "21/10/2024 23:31";
 
     #ifdef __cplusplus
     }
